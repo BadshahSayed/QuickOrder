62039786bd337584de22f7784d15532e3562b204
BadshahSayed <badshahsayed2010@gmail.com>
2026-02-02 16:14:14 +0530

    1 rs

--- src/app/core/data/products.data.ts
+++ src/app/core/data/products.data.ts
@@ -61,7 +61,7 @@ export const PRODUCTS: Product[] = [
         // New Product
         name: "Bombay Gymkhana Stamp",
         description: "150TH ANNIVERSARY OF BOMBAY GYMKHANA Stamp",
-        price: 0.10,
+        price: 1,
         image: "/assets/products/BGM-Stamp.jpg",
         images: ["/assets/products/BGM-Stamp.jpg"]
     },
