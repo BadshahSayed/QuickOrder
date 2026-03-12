5a4958a23e52d7841ab93548fa59726627652193
BadshahSayed <badshahsayed2010@gmail.com>
2026-03-12 14:10:48 +0530

    product divide

--- src/app/core/data/products.data.ts
+++ src/app/core/data/products.data.ts
@@ -17,9 +17,23 @@ export const PRODUCTS: Product[] = [
         description: "The Casablanca Polo—tailored fit, premium fabric, and a commemorative logo that celebrates the Club’s heritage. Available in Classic White and Black.",
         price: 1150,
         image: "/assets/products/polo-white.jpg",
-        images: ["/assets/products/polo-white.jpg", "/assets/products/polo-black.jpg", "/assets/products/catalog-1.jpg"],
+        images: ["/assets/products/polo-white.jpg", "/assets/products/catalog-1.jpg"],
         colors: [
             { name: 'White', class: 'bg-white border-gray-200', code: '#ffffff', image: '/assets/products/polo-white.jpg' },
+            // { name: 'Black', class: 'bg-black border-gray-900', code: '#000000', image: '/assets/products/polo-black.jpg' }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL', '5XL']
+    },
+    {
+        id: 12,
+        // Merged Name: Classic Polo T-Shirt (White & Black)
+        name: "Classic Men’s Polo T-Shirt",
+        description: "The Casablanca Polo—tailored fit, premium fabric, and a commemorative logo that celebrates the Club’s heritage. Available in Classic White and Black.",
+        price: 1150,
+        image: "/assets/products/polo-black.jpg",
+        images: ["/assets/products/polo-black.jpg", "/assets/products/catalog-1.jpg"],
+        colors: [
+            //{ name: 'White', class: 'bg-white border-gray-200', code: '#ffffff', image: '/assets/products/polo-white.jpg' },
             { name: 'Black', class: 'bg-black border-gray-900', code: '#000000', image: '/assets/products/polo-black.jpg' }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL', '5XL']
@@ -31,9 +45,21 @@ export const PRODUCTS: Product[] = [
         description: "Skilfully woven in pure silk and finished with the exclusive 150 Years logo. A refined tribute to the Club’s legacy",
         price: 1665,
         image: "/assets/products/tie-beige.jpg", // Yellow/Beige as default
-        images: ["/assets/products/tie-beige.jpg", "/assets/products/tie-blue.jpg", "/assets/products/catalog-2.jpg"],
+        images: ["/assets/products/tie-beige.jpg", "/assets/products/catalog-2.jpg"],
         colors: [
             { name: 'Yellow', class: 'bg-yellow-200 border-yellow-300', code: '#fef3c7', image: '/assets/products/tie-beige.jpg' },
+            //{ name: 'Black', class: 'bg-gray-900 border-black', code: '#111827', image: '/assets/products/tie-blue.jpg' }
+        ]
+    }, {
+        id: 13,
+        // Merged Name: Zodiac Collection: Silk Tie (Yellow & Black)
+        name: "Zodiac Collection: Silk Tie",
+        description: "Skilfully woven in pure silk and finished with the exclusive 150 Years logo. A refined tribute to the Club’s legacy",
+        price: 1665,
+        image: "/assets/products/tie-blue.jpg", // Yellow/Beige as default
+        images: ["/assets/products/tie-blue.jpg", "/assets/products/catalog-2.jpg"],
+        colors: [
+            //{ name: 'Yellow', class: 'bg-yellow-200 border-yellow-300', code: '#fef3c7', image: '/assets/products/tie-beige.jpg' },
             { name: 'Black', class: 'bg-gray-900 border-black', code: '#111827', image: '/assets/products/tie-blue.jpg' }
         ]
     },
